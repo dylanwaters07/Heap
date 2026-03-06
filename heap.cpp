c45f12712bbcf3a688674f33c9d3d27f4b388a66
DylanW007 <453480@bsd48.org>
2026-03-06 11:54:43 -0800

    Save 3

--- heap.cpp
+++ heap.cpp
@@ -1,36 +1,56 @@
 #include <iostream>
-#include <heap,h>
+#include "heap.h"
 
 using namespace std;
 
 //https://www.geeksforgeeks.org/cpp/implement-heap-in-c/
 // Used for insert logic
-void heap::heapBuild(int arr[],int num){
-   // Add
+void heap::add(int num){
+    if (size >= HEAPSIZE){ // Stop adding if array is at max numbers
+        return;
+    }
+    
+    
+    
 }
 
-void heap::heapFileBuild(){
-
+void heap::heapBuild(){
+   /*for(...logic){
+       restructHeap(i); // Structure the numbers
+   }*/
 }
 
-void heap::clearMax(int arr[], int num){
-  int last = arr[num-1]; // Get last number
-  arr[0] = last; // Replace head with last
-  num = (num-1); // Set num -1 since we lost a number.
-  restruct remove // Run restruct
+void heap::heapFileBuild(const char* filename){
+    cout << "Building from file" << endl;
+}
+
+void heap::clearMax(){
+  if (size == 0){
+      return;
+  }
+  arr[0] = arr[size-1];
+  size = (size-1); // Set num -1 since we lost a number.
+  restructHeap(0); // Run restruct
 }
 
 //https://www.geeksforgeeks.org/dsa/insertion-and-deletion-in-heaps/
 void heap::restructHeap(int i){
   int largest = i; // First value is the largest
-  // Left is equal to 2*i+1
-  // Right is equal to 2*i+2
-
-  //---
-  // Adding, bottom to top (Balance up)
-  // If num is even do (x-2)/2
-  // If num is odd do (x-1)/2
-  //---
+  int left = 2*i+1;
+  int right = 2*i+2;
   
-  // Remove, top to bottom (Balance down)
+  if (left < size && arr[left] > arr[largest]){
+        largest = left;
+  }
+  if (right < size && arr[right] > arr[largest]){
+        largest = right;
+  }
+  if (largest != i) {
+        swap(arr[i], arr[largest]);
+        restructHeap(largest);
+  }
+}
+
+void heap::print(int i){
+    cout << "Printing" << endl;
 }
